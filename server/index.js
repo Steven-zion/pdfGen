e05aa0dc9a341188c71705894a5e72e325f8c806
stevenoketch6@gmail.com <stevenoketch6@gmail.com>
2022-07-14 01:23:14 +0300

    2nd commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import config from './config';
-import { router } from './routes';
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const pdf = require ('html-pdf');
+const cors = require ('cors');
+const pdfTemplate = require('./docs/index.js');
+
+const app = express ();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use (bodyParser.json ());
+app.use (cors ());
+
+//POST - RECEIPT GENERATION AND FETCHING OF DATA
+app.post ('/createReceipt', (req, res) => {
+	pdf.create(pdfTemplate(req.data), {}).toFile('result-receipt', (err)=>{
+		if(err) {
+			return Promise.reject(err);
+		}
+		return Promise.resolve();
+	})
+})
+
+//GET - SETHE GENERATED RECEIPT 
+
+
+app.listen (port, () => {
+	console.log (`Server is running on port ${port}`);
+}
