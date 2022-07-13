26e599ef2b23bdcace00b757850b40b78d8a96e3
stevenoketch6@gmail.com <stevenoketch6@gmail.com>
2022-07-13 22:50:57 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config';
+import { router } from './routes';
