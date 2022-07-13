4b59c57f294a36ae843ec54883c3eb4a399f1045
stevenoketch6@gmail.com <stevenoketch6@gmail.com>
2022-07-14 01:27:48 +0300

    2nd commit

--- server/index.js
+++ server/index.js
@@ -1,30 +1,32 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const pdf = require ('html-pdf');
-const cors = require ('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
 const pdfTemplate = require('./docs/index.js');
 
-const app = express ();
+const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use (bodyParser.json ());
-app.use (cors ());
+app.use(bodyParser.json());
+app.use(cors());
 
 //POST - RECEIPT GENERATION AND FETCHING OF DATA
-app.post ('/createReceipt', (req, res) => {
-	pdf.create(pdfTemplate(req.data), {}).toFile('result-receipt', (err)=>{
-		if(err) {
-			return Promise.reject(err);
-		}
-		return Promise.resolve();
-	})
-})
+app.post('/createReceipt', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+        if (err) {
+            res.send(Promise.reject(err));
+        }
+        res.send(Promise.resolve());
+    });
+});
 
-//GET - SETHE GENERATED RECEIPT 
+// GET - SEND THE GENERATED RECEIPT TO THE CLIENT
+app.get('/fetchReceipt', (req, res) => {
+    res.sendFile(`${_dirname}/result.pdf`);
+});
 
-
-app.listen (port, () => {
-	console.log (`Server is running on port ${port}`);
-}
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
